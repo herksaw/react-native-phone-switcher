76c4e8c92a0eb950380c78b5c7f12eb4718d9f6e
herksaw <herksaw@gmail.com>
2021-10-18 14:34:22 +0800

    changes

--- src/deviceSizes.js
+++ src/deviceSizes.js
@@ -50,11 +50,147 @@ const deviceSizes = {
   //  },
   // },
 
-  'Samsung Galaxy Note20 Ultra (12.9)': {
+  'Galaxy Z Fold 2 (7.6)': {
+    windowPhysicalPixels: {
+      width: 1768,
+      height: 2208,
+      scale: 2,
+    },
+  },
+
+  'Galaxy Note20 Ultra (6.9)': {
     windowPhysicalPixels: {
       width: 1440,
       height: 3088,
       scale: 3,
+    },
+  },
+
+  'Galaxy S20 Ultra (6.9)': {
+    windowPhysicalPixels: {
+      width: 1440,
+      height: 3200,
+      scale: 3,
+    },
+  },
+
+  'Galaxy Note10+ (6.8)': {
+    windowPhysicalPixels: {
+      width: 1440,
+      height: 3040,
+      scale: 3,
+    },
+  },
+
+  'Redmi Note 9 Pro (6.7)': {
+    windowPhysicalPixels: {
+      width: 1080,
+      height: 2400,
+      scale: 2,
+    },
+  },
+
+  'iPhone 12 Pro Max (6.7)': {
+    windowPhysicalPixels: {
+      width: 1284,
+      height: 2778,
+      scale: 3,
+    },
+  },
+
+  'Huawei Mate 20 (6.5)': {
+    windowPhysicalPixels: {
+      width: 1080,
+      height: 2244,
+      scale: 2,
+    },
+  },
+
+  'iPhone XS Max (6.5)': {
+    windowPhysicalPixels: {
+      width: 1242,
+      height: 2688,
+      scale: 3,
+    },
+  },
+
+  'Redmi Note 8 Pro (6.5)': {
+    windowPhysicalPixels: {
+      width: 1080,
+      height: 2340,
+      scale: 2,
+    },
+  },
+
+  'Nova 7i (6.4)': {
+    windowPhysicalPixels: {
+      width: 1080,
+      height: 2310,
+      scale: 2,
+    },
+  },
+
+  'Galaxy Note10 (6.3)': {
+    windowPhysicalPixels: {
+      width: 1080,
+      height: 2280,
+      scale: 3,
+    },
+  },
+
+  'iPhone XR (6.1)': {
+    windowPhysicalPixels: {
+      width: 828,
+      height: 1792,
+      scale: 2,
+    },
+  },
+
+  'iPhone 12 (6.1)': {
+    windowPhysicalPixels: {
+      width: 1170,
+      height: 2532,
+      scale: 3,
+    },
+  },
+
+  'iPhone X (5.8)': {
+    windowPhysicalPixels: {
+      width: 1125,
+      height: 2436,
+      scale: 3,
+    },
+  },
+
+  'Galaxy Note5 (5.7)': {
+    windowPhysicalPixels: {
+      width: 1440,
+      height: 2560,
+      scale: 3,
+    },
+  },
+
+  'Mi Note (5.7)': {
+    windowPhysicalPixels: {
+      width: 1080,
+      height: 1920,
+      scale: 2,
+    },
+  },
+
+  'Galaxy Note (5.3)': {
+    windowPhysicalPixels: {
+      width: 800,
+      height: 1280,
+      scale: 2,
+    },
+  },
+
+  'Apple iPhone 8 (4.7)': {
+    windowPhysicalPixels: {
+      width: 750,
+      height: 1334,
+      scale: 2,
     },
   },
 };
